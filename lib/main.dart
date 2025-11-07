4865e9fb61b36db40663608b6460e5f983015562
talha270 <144779558+talha270@users.noreply.github.com>
2025-11-07 15:11:41 +0500

    inital commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,94 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:video_player/video_player.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
 
-void main() {
-  runApp(const MyApp());
+class VideoEditorScreen extends StatefulWidget {
+  @override
+  _VideoEditorScreenState createState() => _VideoEditorScreenState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _VideoEditorScreenState extends State<VideoEditorScreen> {
+  File? _videoFile;
+  VideoPlayerController? _controller;
+  bool _isProcessing = false;
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  Future<void> _pickVideo() async {
+    final pickedFile = await ImagePicker().pickVideo(source: ImageSource.gallery);
+    if (pickedFile != null) {
+      setState(() {
+        _videoFile = File(pickedFile.path);
+        _controller = VideoPlayerController.file(_videoFile!)
+          ..initialize().then((_) {
+            setState(() {});
+            _controller!.play();
+          });
+      });
+    }
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  Future<void> _trimVideo() async {
+    if (_videoFile == null) return;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+    setState(() => _isProcessing = true);
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+    final dir = await getTemporaryDirectory();
+    final outputPath = '${dir.path}/trimmed_video.mp4';
 
-  final String title;
+    String command = '-i ${_videoFile!.path} -ss 00:00:02 -t 00:00:05 -c copy $outputPath';
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+    await FFmpegKit.execute(command);
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _videoFile = File(outputPath);
+      _controller = VideoPlayerController.file(_videoFile!)
+        ..initialize().then((_) {
+          setState(() {});
+          _controller!.play();
+        });
+      _isProcessing = false;
     });
   }
 
   @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      appBar: AppBar(title: Text("Video Editor")),
+      body: Column(
+        children: [
+          if (_controller != null && _controller!.value.isInitialized)
+            AspectRatio(
+              aspectRatio: _controller!.value.aspectRatio,
+              child: VideoPlayer(_controller!),
+            )
+          else
+            Container(height: 200, color: Colors.grey[300], child: Center(child: Text("No Video Selected"))),
+
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: _pickVideo,
+            child: Text("Pick Video"),
+          ),
+          ElevatedButton(
+            onPressed: _isProcessing ? null : _trimVideo,
+            child: _isProcessing ? CircularProgressIndicator() : Text("Trim Video (First 5s)"),
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+void main() {
+  runApp(MaterialApp(
+    home: VideoEditorScreen(),
+  ));
 }
